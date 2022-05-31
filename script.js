6b213eae5968e2b09a23e419a34e35d1b8cdadf8
Saber Heydari <Silverethical@gmail.com>
2022-05-31 19:33:22 +0430

    add font, and progress to automate questions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// script
+let userAnswers = [],
+  correctAnswers = [
+    3, 1, 5, 5, 2, 1, 2, 2, 2, 6, 4, 1, 4, 7, 2, 3, 1, 6, 5, 8, 4, 4, 7, 6, 
+    4, 7, 7, 3, 2, 8,
+  ];
+
+function userInput() {
+  window.onclick = (e) => {
+    userAnswers.push(e.target.id); // get the element ID
+  };
+}
 
 document.querySelector("body").innerHTML = `
 <section id="whole-question">
@@ -11,27 +21,25 @@ let htmlWholeQuestion = document.querySelector("#whole-question"),
   htmlQuestion = document.querySelector("#question"),
   htmlAnswers = document.querySelector("#answers");
 
-function applyQuestion(questionNumber) {
-  if (questionNumber >= 1) {
+function applyQuestion(qNum) {
+  // qNum == Question Number
+  if (qNum >= 1) {
     let addQuestion = document.createElement("img");
     addQuestion.setAttribute(
       "src",
-      "./Images/" + questionNumber + "/test" + questionNumber + ".png"
+      "./Images/" + qNum + "/test" + qNum + ".png"
     );
     htmlQuestion.appendChild(addQuestion);
 
-    for (let answerNumber = 1; answerNumber < 7; answerNumber++) {
+    for (let aNum = 1; aNum <= 6; aNum++) {
+      // aNum == Answer Number
       let addAnswers = document.createElement("img");
+      addAnswers.setAttribute("id", aNum);
       addAnswers.setAttribute(
         "src",
-        "./Images/" +
-          questionNumber +
-          "/" +
-          questionNumber +
-          "-" +
-          answerNumber +
-          ".png"
+        "./Images/" + qNum + "/" + qNum + "-" + aNum + ".png"
       );
+      addAnswers.addEventListener("click", userInput);
       htmlAnswers.appendChild(addAnswers);
     }
   }
