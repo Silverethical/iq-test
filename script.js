16b3efb0aedb78780da1a75583c2fc71bffce1c8
Ghazaleh.developer <ghazalehrishehrizadeh@gmail.com>
2022-06-02 09:43:30 +0430

    background style change
    back ground change

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let userAnswers = [],
     7, 7, 3, 2, 8,
   ];
 
-document.querySelector("body").innerHTML = `
-<section id="whole-question">
+document.querySelector("body").innerHTML =
+ `<section id="whole-question">
     <p>Please choose the right answer by clicking on it</p>
     <div id="question"></div>
     <div id="answers"></div>
